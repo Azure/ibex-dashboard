4a21b9f6836b98c03ae13729a6cb7bc83ed2326d
Erik Schlegel <erik.schlegel@gmail.com>
2016-04-29 17:49:20 -0400

    Adding libya geojson files which covers regions, localities and neigborhoods across the country. Also added various examples on how to render geojson data from OSM on the heatmap

---
+++ osmShapes/OSMBorderDescedents.js
@@ -0,0 +1,101 @@
+var libyanBorders = [
+    {
+        regionId: 85673631, descendents: [
+        421205149, 421199025
+    ]},
+    {
+        regionId: 85673627, descendents: [
+        421189699, 421177163
+    ]},
+    {
+        regionId: 85673663, descendents: [
+        421189705, 421201967
+    ]},
+    {
+        regionId: 85673567, descendents: [
+        421198079
+    ]},
+    {
+        regionId: 85673625, descendents: [
+        421174669, 421196729
+    ]},
+    {
+        regionId: 85673605, descendents: [
+    ]},
+    {
+        regionId: 85673571, descendents: [
+        421169929
+    ]},
+    {
+        regionId: 85673665, descendents: [
+        421189701
+    ]},
+    {
+        regionId: 85673621, descendents: [
+        421187469, 85902891, 85902893, 85902901, 85902895, 85902897, 421194651
+    ]},
+    {
+        regionId: 85673669, descendents: [
+        421199607
+    ]},
+    
+    {
+        regionId: 85673575, descendents: [
+        421172891, 421183239, 421190857, 421189687
+    ]},
+    {
+        regionId: 85673607, descendents: [
+        421194649, 421196731, 421183237
+    ]},
+    {
+        regionId: 85673579, descendents: [
+    ]},
+    {
+        regionId: 85673611, descendents: [
+        421189691, 421199025
+    ]},
+    {
+        regionId: 85673635, descendents: [
+        85902889, 421186127, 421183743, 421192375, 421190913, 85902899, 421189707, 421199765
+    ]},
+    {
+        regionId: 85673563, descendents: [
+        421187479
+    ]},
+    {
+        regionId: 85673585, descendents: [
+        421189703, 421189689
+    ]},
+    {
+        regionId: 85673593, descendents: [
+        421168935
+    ]},
+    {
+        regionId: 85673615, descendents: [
+        421189697, 421197205, 421197243, 421200689, 421201969
+    ]},
+    {
+        regionId: 85673589, descendents: [
+        421189693
+    ]},
+    {
+        regionId: 85673597, descendents: [
+        421180019
+    ]},
+    {
+        regionId: 421197205, descendents: [
+    ]},
+    {
+        regionId: 85673659, descendents: [
+            421199027
+    ]},
+    {
+        regionId: 421190807, descendents: [
+    ]},
+    {
+        regionId: 421200689, descendents: [
+    ]},
+    {
+        regionId: 421172683, descendents: [
+    ]}
+];
