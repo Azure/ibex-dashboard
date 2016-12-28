ba55e8c678214ce1f4482e38dc12698924a1d0c1
Erik Schlegel <erisch@microsoft.com>
2016-12-28 14:13:11 -0500

    Pathing a variety of bug fixes to the news feed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -76,7 +76,9 @@ export const Dashboard = React.createClass({
                 <div className="row" id="contentRow">
                   <div>
                             <div className="col-md-3 termBrowserContainer">
-                                <SentimentTreeview {...this.props} language={this.state.language} />
+                                <SentimentTreeview {...this.props} 
+                                                enabledTerms={Array.from(this.state.termFilters)}
+                                                language={this.state.language} />
                             </div>
                             <div className="col-md-6 heatmapContainer">
                             <div className="row">
