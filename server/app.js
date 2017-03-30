500390b2c6a89ade8041e434dd1eab1b03c32891
morsh <mor.shemesh@gmail.com>
2017-03-30 17:25:36 +0300

    enabling no auth

--- server/app.js
+++ server/app.js
@@ -8,33 +8,23 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const passportAzureAD = require('passport-azure-ad');
-const config = require('./config.private');
 
 const OIDCStrategy = passportAzureAD.OIDCStrategy;
 
-// Passport session setup. (Section 2)
+let config = null;
+let authEnabled = false;
 
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.email);
-});
-
-passport.deserializeUser(function(id, done) {
-  findByEmail(id, function (err, user) {
-    done(err, user);
-  });
-});
+if (fs.existsSync(path.join(__dirname, 'config.private.js'))) {
+  config = require('./config.private');
+  authEnabled = true;
+}
 
 // array to hold logged in users
 var users = [];
-
 var findByEmail = function(email, fn) {
   for (var i = 0, len = users.length; i < len; i++) {
     var user = users[i];
-   console.info('we are using user: ', user);
+  console.info('we are using user: ', user);
     if (user.email === email) {
       return fn(null, user);
     }
@@ -42,47 +32,65 @@ var findByEmail = function(email, fn) {
   return fn(null, null);
 };
 
-// Use the OIDCStrategy within Passport. (Section 2) 
-// 
-//   Strategies in passport require a `validate` function, which accept
-//   credentials (in this case, an OpenID identifier), and invoke a callback
-//   with a user object.
-passport.use(new OIDCStrategy({
-    redirectUrl: config.creds.redirectUrl,
-    allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
-    realm: config.creds.realm,
-    clientID: config.creds.clientID,
-    clientSecret: config.creds.clientSecret,
-    oidcIssuer: config.creds.issuer,
-    identityMetadata: config.creds.identityMetadata,
-    skipUserProfile: config.creds.skipUserProfile,
-    responseType: config.creds.responseType,
-    responseMode: config.creds.responseMode,
-    validateIssuer: config.creds.validateIssuer
-  },
-  function(iss, sub, profile, accessToken, refreshToken, done) {
+if (authEnabled) {
+  // Passport session setup. (Section 2)
 
-    profile.email = profile.email || profile.upn;
+  //   To support persistent login sessions, Passport needs to be able to
+  //   serialize users into and deserialize users out of the session.  Typically,
+  //   this will be as simple as storing the user ID when serializing, and finding
+  //   the user by ID when deserializing.
+  passport.serializeUser(function(user, done) {
+    done(null, user.email);
+  });
 
-    if (!profile.email) {
-      return done(new Error("No email found"), null);
-    }
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      findByEmail(profile.email, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          // "Auto-registration"
-          users.push(profile);
-          return done(null, profile);
-        }
-        return done(null, user);
-      });
+  passport.deserializeUser(function(id, done) {
+    findByEmail(id, function (err, user) {
+      done(err, user);
     });
-  }
-));
+  });
+
+  // Use the OIDCStrategy within Passport. (Section 2) 
+  // 
+  //   Strategies in passport require a `validate` function, which accept
+  //   credentials (in this case, an OpenID identifier), and invoke a callback
+  //   with a user object.
+  passport.use(new OIDCStrategy({
+      redirectUrl: config.creds.redirectUrl,
+      allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+      realm: config.creds.realm,
+      clientID: config.creds.clientID,
+      clientSecret: config.creds.clientSecret,
+      oidcIssuer: config.creds.issuer,
+      identityMetadata: config.creds.identityMetadata,
+      skipUserProfile: config.creds.skipUserProfile,
+      responseType: config.creds.responseType,
+      responseMode: config.creds.responseMode,
+      validateIssuer: config.creds.validateIssuer
+    },
+    function(iss, sub, profile, accessToken, refreshToken, done) {
+
+      profile.email = profile.email || profile.upn;
+
+      if (!profile.email) {
+        return done(new Error("No email found"), null);
+      }
+      // asynchronous verification, for effect...
+      process.nextTick(function () {
+        findByEmail(profile.email, function(err, user) {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            // "Auto-registration"
+            users.push(profile);
+            return done(null, profile);
+          }
+          return done(null, user);
+        });
+      });
+    }
+  ));
+}
 
 const app = express();
 
@@ -93,8 +101,10 @@ app.use(bodyParser.urlencoded({ extended : true }));
 
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
-app.use(passport.initialize());
-app.use(passport.session());
+if (authEnabled) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+}
 
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
@@ -226,7 +236,7 @@ module.exports = app;
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
+  if (!authEnabled || req.isAuthenticated()) { return next(); }
   res.redirect('/auth/login')
 }
 
