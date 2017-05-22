d6f7cb3c9ab0cd70ca038b8a01ef2b378413a2f4
Clemens Wolff <clewolff@microsoft.com>
2017-05-22 08:53:51 -0700

    Add connection setup instructions

--- src/data-sources/connections/graphql.tsx
+++ src/data-sources/connections/graphql.tsx
@@ -31,6 +31,17 @@ class GraphQLConnectionEditor extends ConnectionEditor<IConnectionProps, any> {
     return (
       <div>
         <h2 style={{ float: 'left', padding: 9 }}>GraphQL Connection</h2>
+        <InfoDrawer
+          width={300}
+          title="Authentication"
+          buttonIcon="help"
+          buttonTooltip="Click here to learn more about authentication"
+        >
+          <div>
+            Just enter the URL of the GraphQL service you wish to query below.
+            Currently only publicly accessible GraphQL endpoints are supported.
+          </div>
+        </InfoDrawer>
         <TextField
           id="serviceUrl"
           label={'Service URL'}
