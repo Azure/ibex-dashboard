44065fe6762965ddaeaefc6739d7e65189706cbf
morsh <mor.shemesh@gmail.com>
2017-05-29 18:40:27 +0300

    updating build

--- server/dashboards/preconfigured/bot-framework.ts
+++ server/dashboards/preconfigured/bot-framework.ts
@@ -824,7 +824,7 @@ export const config: IDashboardConfig = /*return*/ {
             `
           },
           calculated: ({ values }) => {
-            let sentimentValue = Math.round(((values && values.length && values[0].avg_sentiment) || 0) * 100, 1);
+            let sentimentValue = +(((values && values.length && values[0].avg_sentiment) || 0) * 100, 1).toFixed(1);
             return {
               "sentiment-value": sentimentValue + '%',
               "sentiment-height": sentimentValue + '%',
