d88189430b1a41b6e4f79df5a59b0b3a018e8db6
morsh <mor.shemesh@gmail.com>
2017-08-15 15:38:13 +0300

    update test cases

--- client/src/tests/data-formats/formats/timeline.ts
+++ client/src/tests/data-formats/formats/timeline.ts
@@ -11,19 +11,19 @@ export default <IFormatTest>{
   },
   state: {
     values: [
-      { timestamp: '2015-01-01 00:00:01', line: 'red', value: 40 },
-      { timestamp: '2015-01-02 00:00:01', line: 'red', value: 50 },
-      { timestamp: '2015-01-03 00:00:01', line: 'red', value: 60 },
-      { timestamp: '2015-01-01 00:00:01', line: 'blue', value: 10 },
-      { timestamp: '2015-01-02 00:00:01', line: 'blue', value: 77 },
-      { timestamp: '2015-01-03 00:00:01', line: 'blue', value: 30 }
+      { timestamp: '2015-01-01T00:00:01Z', line: 'red', value: 40 },
+      { timestamp: '2015-01-02T00:00:01Z', line: 'red', value: 50 },
+      { timestamp: '2015-01-03T00:00:01Z', line: 'red', value: 60 },
+      { timestamp: '2015-01-01T00:00:01Z', line: 'blue', value: 10 },
+      { timestamp: '2015-01-02T00:00:01Z', line: 'blue', value: 77 },
+      { timestamp: '2015-01-03T00:00:01Z', line: 'blue', value: 30 }
     ]
   },
   expected: {
     "graphData": [
-      {"blue": 10, "red": 40, "time": "Wed, 31 Dec 2014 22:00:01 GMT"}, 
-      {"blue": 77, "red": 50, "time": "Thu, 01 Jan 2015 22:00:01 GMT"}, 
-      {"blue": 30, "red": 60, "time": "Fri, 02 Jan 2015 22:00:01 GMT"}
+      {"blue": 10, "red": 40, "time": "Thu, 01 Jan 2015 00:00:01 GMT"}, 
+      {"blue": 77, "red": 50, "time": "Fri, 02 Jan 2015 00:00:01 GMT"}, 
+      {"blue": 30, "red": 60, "time": "Sat, 03 Jan 2015 00:00:01 GMT"}
     ], 
     "lines": ["red", "blue"], 
     "pieData": [
